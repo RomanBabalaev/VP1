d7eb6f5f02733267198ec075b358c3779b93ba1b
RomanBabalaev <36676728+RomanBabalaev@users.noreply.github.com>
2018-04-02 03:25:54 +0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    "db" => [
+        "host" => "localhost",
+        "database" => "burger",
+        "user" => "root",
+        "password" => "",
+        "charset" => "utf8"
+    ]
+
+];
